7e43b874f903affde3d0a2f90b162098f0283d2f
c_wgao <gao_ww@codeaurora.org>
2015-12-14 16:30:01 +0800

    CMFileManager: Fix CMFileManger force close.
    When press back key in SearchActivity, the adapter data will be
    clear. But the adapter keeps loading which cause this bug happen.
    
    When exit SearchActivity,set the listview adpter to null to stop
    adapter loading.
    
    CRs-Fixed: 950597

--- src/com/cyanogenmod/filemanager/activities/SearchActivity.java
+++ src/com/cyanogenmod/filemanager/activities/SearchActivity.java
@@ -1370,6 +1370,7 @@ public class SearchActivity extends Activity
     private void exit() {
         if (this.mSearchListView.getAdapter() != null) {
             ((SearchResultAdapter)this.mSearchListView.getAdapter()).dispose();
+            this.mSearchListView.setAdapter(null);
         }
         finish();
     }
